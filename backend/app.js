0dbf6935faf95b7fdbfa7dbfcf32598b178573fe
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2025-07-31 14:27:35 +0530

    Changes in the app.js

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const authController = require("./controllers/authController.js")
-// const AuthSchemaModel = require("./models/authSchema.js");
+const authController = require("./controllers/authController.js");
 const registerController = require('./controllers/registerController.js');
-// const blogSchemaModel = require('./models/blogSchema.js');
 const blogController = require('./controllers/blogController.js');
 const blogAddController = require('./controllers/blogAddController.js');
 const likeController = require('./controllers/likeController.js');
@@ -19,8 +17,6 @@ const myPostController = require('./controllers/myPostController.js');
 const { CommentDeleteController, CommentFlagController, CommentReplyController, CommentApproveController } = require('./controllers/adminController.js');
 const { AdminBlogRestore, AdminBlogUnarchive, AdminBlogPermanentDelete, AdminBlogDelete, AdminBlogArchive } = require('./controllers/adminStatusController.js');
 
-
-
 const corsOptions = {
   origin: 'https://my-app-frontend-g1f2.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
@@ -31,74 +27,14 @@ app.use(express.json());
 
 const URI = process.env.MONGO_URI || "mongodb+srv://bhadriprabhu:bhadri%402006@bhadri.osdxvju.mongodb.net/blog_portal?retryWrites=true&w=majority&appName=bhadri";
 
-
 const connectDB = async () => {
-    try {
-        await mongoose.connect(URI);
-        console.log("DB connected successfully")
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-// async function run() {
-//     try{
-//         const user = await AuthSchemaModel.create({
-//             user: "Bhadri",
-//             email: "bhadri@gmail.com",
-//             password: "1234",
-//             // role: "admin",
-//         })
-//         console.log(user);
-
-//         // user.save();
-//     }catch(err){
-//         console.log(err);
-//     }
-// }
-
-// run();
-
-// async function run() {
-//     try {
-//         const blog = await blogSchemaModel({
-//             user: new mongoose.Types.ObjectId("6883bf95edac67b4473ff771"),
-//             email: "prabhu@gmail.com",
-//             title: "Deep Learning with Transformers",
-//             desc: "This article delves into how transformers have changed NLP and other domains, covering self-attention, positional encoding, and BERT-like architectures.",
-//             like: 245,
-//             liked: false,
-//             saved: false,
-//             comments: [
-//                 {
-//                     user: new mongoose.Types.ObjectId("688612d955e7e177f1c0da42"),
-//                     value: "Machine learning is the future.",
-//                     reply: [
-//                         {
-//                             user: new mongoose.Types.ObjectId("688612bc55e7e177f1c0da38"),
-//                             value: "Yes, definitely!",
-//                         },
-//                         {
-//                             user: new mongoose.Types.ObjectId("688612d955e7e177f1c0da42"),
-//                             value: "Absolutely agree with you.",
-//                         },
-//                     ],
-//                 },
-//             ],
-//         });
-
-//         await blog.save();
-
-
-//         console.log("Blog saved:", blog);
-//     } catch (err) {
-//         console.error("Error saving blog:", err);
-//     }
-// }
-
-// run();
-
-
+  try {
+    await mongoose.connect(URI);
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+};
 
 app.post("/", authController);
 app.post("/register", registerController);
@@ -108,8 +44,8 @@ app.post("/blog/add", blogAddController);
 app.post("/blog/:id/comment", addCommentBlog);
 app.post("/blog/:id/comment/:commentId/reply", addReplyComment);
 app.post("/blog/save", saveController);
-app.post("/blog/mypost",myPostController);
-app.get("/blog/saved",savedController);
+app.post("/blog/mypost", myPostController);
+app.get("/blog/saved", savedController);
 app.get("/blog/liked", likedBlogController);
 app.post("/blog/comments/delete", CommentDeleteController);
 app.post("/blog/comments/flag", CommentFlagController);
@@ -121,11 +57,7 @@ app.post("/blog/permanent-delete", AdminBlogPermanentDelete);
 app.post("/blog/delete", AdminBlogDelete);
 app.post("/blog/archive", AdminBlogArchive);
 
-
-
-
-
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server is running in the port ${PORT}`)
+  connectDB();
+  console.log(`Server is running in the port ${PORT}`);
 });
