ea9f43d68f7baa7c5c6af9b86a795edeb63931da
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2025-07-31 14:02:13 +0530

    Cors error in the backend again

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const authController = require("./controllers/authController.js")
 // const AuthSchemaModel = require("./models/authSchema.js");
 const registerController = require('./controllers/registerController.js');
-const blogSchemaModel = require('./models/blogSchema.js');
+// const blogSchemaModel = require('./models/blogSchema.js');
 const blogController = require('./controllers/blogController.js');
 const blogAddController = require('./controllers/blogAddController.js');
 const likeController = require('./controllers/likeController.js');
@@ -18,14 +18,13 @@ const likedBlogController = require('./controllers/likedBlogController.js');
 const myPostController = require('./controllers/myPostController.js');
 const { CommentDeleteController, CommentFlagController, CommentReplyController, CommentApproveController } = require('./controllers/adminController.js');
 const { AdminBlogRestore, AdminBlogUnarchive, AdminBlogPermanentDelete, AdminBlogDelete, AdminBlogArchive } = require('./controllers/adminStatusController.js');
-// const sortByController = require('./controllers/sortByController.js');
 
 
 
 const corsOptions = {
-  origin: 'https://my-app-frontend-g1f2.onrender.com', // Match your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'OPTIONS'], // Allow OPTIONS for preflight
-  allowedHeaders: ['Content-Type'], // Match request headers
+  origin: 'https://my-app-frontend-g1f2.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
 };
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -60,42 +59,42 @@ const connectDB = async () => {
 
 // run();
 
-async function run() {
-    try {
-        const blog = await blogSchemaModel({
-            user: new mongoose.Types.ObjectId("6883bf95edac67b4473ff771"),
-            email: "prabhu@gmail.com",
-            title: "Deep Learning with Transformers",
-            desc: "This article delves into how transformers have changed NLP and other domains, covering self-attention, positional encoding, and BERT-like architectures.",
-            like: 245,
-            liked: false,
-            saved: false,
-            comments: [
-                {
-                    user: new mongoose.Types.ObjectId("688612d955e7e177f1c0da42"),
-                    value: "Machine learning is the future.",
-                    reply: [
-                        {
-                            user: new mongoose.Types.ObjectId("688612bc55e7e177f1c0da38"),
-                            value: "Yes, definitely!",
-                        },
-                        {
-                            user: new mongoose.Types.ObjectId("688612d955e7e177f1c0da42"),
-                            value: "Absolutely agree with you.",
-                        },
-                    ],
-                },
-            ],
-        });
+// async function run() {
+//     try {
+//         const blog = await blogSchemaModel({
+//             user: new mongoose.Types.ObjectId("6883bf95edac67b4473ff771"),
+//             email: "prabhu@gmail.com",
+//             title: "Deep Learning with Transformers",
+//             desc: "This article delves into how transformers have changed NLP and other domains, covering self-attention, positional encoding, and BERT-like architectures.",
+//             like: 245,
+//             liked: false,
+//             saved: false,
+//             comments: [
+//                 {
+//                     user: new mongoose.Types.ObjectId("688612d955e7e177f1c0da42"),
+//                     value: "Machine learning is the future.",
+//                     reply: [
+//                         {
+//                             user: new mongoose.Types.ObjectId("688612bc55e7e177f1c0da38"),
+//                             value: "Yes, definitely!",
+//                         },
+//                         {
+//                             user: new mongoose.Types.ObjectId("688612d955e7e177f1c0da42"),
+//                             value: "Absolutely agree with you.",
+//                         },
+//                     ],
+//                 },
+//             ],
+//         });
 
-        await blog.save();
+//         await blog.save();
 
 
-        console.log("Blog saved:", blog);
-    } catch (err) {
-        console.error("Error saving blog:", err);
-    }
-}
+//         console.log("Blog saved:", blog);
+//     } catch (err) {
+//         console.error("Error saving blog:", err);
+//     }
+// }
 
 // run();
 
@@ -105,7 +104,6 @@ app.post("/", authController);
 app.post("/register", registerController);
 app.put("/blog/like", likeController);
 app.get("/blog", blogController);
-// app.get("/blog?sortBy=saved", sortByController);
 app.post("/blog/add", blogAddController);
 app.post("/blog/:id/comment", addCommentBlog);
 app.post("/blog/:id/comment/:commentId/reply", addReplyComment);
@@ -129,6 +127,5 @@ app.post("/blog/archive", AdminBlogArchive);
 
 app.listen(PORT, () => {
     connectDB();
-    // studentInfo();
     console.log(`Server is running in the port ${PORT}`)
 });
